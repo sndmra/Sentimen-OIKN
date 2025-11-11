72202913f7a6d17b14ea47d54e5161149806f98d
WonderKid <daffadamara.ixd@gmail.com>
2025-11-11 23:37:51 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  output: "export", // ✅ ini yang penting
-  basePath: "/Sentimen-OIKN", // ✅ supaya path di GitHub Pages benar
+  output: "export",
+  basePath: isProd ? "/Sentimen-OIKN" : "", // ✅ aktif hanya di production
+  assetPrefix: isProd ? "/Sentimen-OIKN/" : "", // ✅ agar path file statis juga benar
   images: {
-    unoptimized: true, // ✅ untuk static export
+    unoptimized: true,
   },
 };
 
