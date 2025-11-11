f7c79f7233631ec1e1386fe1a4391b31c5c2d4aa
sndmra <sndmra@users.noreply.github.com>
2025-11-11 08:12:47 +0000

    deploy: dfd9e43730b68b9f8e15077be5d0511688d7231c

---
+++ _next/static/umU0zMCkhVxGMpFBuoviB/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Sentimen-OIKN//_next/:path+",
+        "destination": "/Sentimen-OIKN/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
