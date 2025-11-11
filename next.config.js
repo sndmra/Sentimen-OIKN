db055771be0a030191e51d2003c27f2885619a01
WonderKid <daffadamara.ixd@gmail.com>
2025-11-11 23:00:24 +0800

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
-  basePath: '/Sentimen-OIKN',
-  assetPrefix: '/Sentimen-OIKN/',
-}
-
-module.exports = nextConfig
